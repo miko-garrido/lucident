ceaf5af5799aa7f7b9b857613fe99de5f8584651
miko-garrido <69011999+miko-garrido@users.noreply.github.com>
2025-05-03 13:26:41 +0800

    Move ADK cofiguration to .env as per documentation

--- main.py
+++ main.py
@@ -17,4 +17,4 @@ app: FastAPI = get_fast_api_app(
 )
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
